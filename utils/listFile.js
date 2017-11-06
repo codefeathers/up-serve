6d7523b3fb21b40dcc3f208bb7bc3e62a7520f3f
shunmusanjay <sanjayshanmu7@gmail.com>
2017-11-07 03:52:39 +0530

    listFile.js delete line if domain | outPort exist

--- utils/listFile.js
+++ utils/listFile.js
@@ -14,9 +14,47 @@ function appendToList(domain, outPort, inPort) {
     } else {
         var addr = domain + ':' + outPort + '  ' + 'static' + EOL;
     }
+    //reads file to check if domain already exist
+    var data = fs.readFileSync(listFilePath);
 
-    //wirtes to the file in path
-    fs.appendFileSync(listFilePath, addr);
+    var dataString = data.toString().split("\n");
+
+    var isDomainUnique = dataString.search(domain); // returns -1 if not found else line count
+    var isOutPortUnique = dataString.search(outPort); // returns -1 if not found else line count
+
+    if (isDomainUnique == -1 && isOutPortUnique == -1) {
+        //wirtes to the file in path
+        fs.appendFileSync(listFilePath, addr);
+    } else if (isDomainUnique == -1 || isOutPortUnique == -1) {
+        if (isDomainUnique == -1) {
+            var deleteOutPortLine = dataString.slice(isOutPortUnique).join('\n');
+            fs.appendFileSync(listFilePath, deleteOutPortLine);
+            fs.appendFileSync(listFilePath, addr);
+
+        } else {
+            var deleteDomainLine = dataString.slice(isDomainUnique).join('\n');
+            fs.appendFileSync(listFilePath, deleteDomainLine);
+            fs.appendFileSync(listFilePath, addr);
+        }
+
+    } else {
+        var deleteDomainLine = dataString.slice(isDomainUnique).join('\n');
+        fs.appendFileSync(listFilePath, deleteDomainLine);
+
+        var data = fs.readFileSync(listFilePath);
+
+        var dataString = data.toString().split("\n");
+        var isOutPortUnique = dataString.search(outPort); // returns -1 if not found else line count
+
+        var deleteOutPortLine = dataString.slice(isOutPortUnique).join('\n');
+        fs.appendFileSync(listFilePath, deleteOutPortLine);
+
+        fs.appendFileSync(listFilePath, addr);
+
+    }
+
+
+
 }
 
 module.exports = appendToList;
